f7eebe97e057d414a5a8ed6634cb44ef5615f822
Kaythay <kathleenxw@gmail.com>
2017-11-18 17:16:20 -0500

    Add Basic Logic for Getting Weather Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title : string = 'this app';
+  code : string = 'CAON0512'; //default is Ottawa
+  keyword : string = 'Ottawa';
+  weatherApi = null;
+
+  ngOnInit() {
+    //init properties
+    this.title = 'this app!';
+    this.weatherApi = axios.create({
+      baseURL : 'https://hackathon.pic.pelmorex.com/api'
+    });
+    this.sendWeatherRequest();
+  }
+
+  receiveWeatherResponse(response){
+    this.code = response.data.code;
+    console.log('Receieved weather data for: ' + response.data.adLocation + '!\n');
+    console.log(response);
+  }
+
+  sendWeatherRequest(/*location : string*/){
+    //TODO: check that location is a valid keyword
+    var self = this;
+    console.log('Requested weather data for: ' + 'Ottawa' + '...');
+    this.weatherApi.get('/search/string', {
+      params: {
+        keyword: 'Ottawa'
+        //response type is json by default
+      }
+    }).then(function(response){
+      self.receiveWeatherResponse(response);
+    });
+  }
 }
